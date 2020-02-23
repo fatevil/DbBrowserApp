92d53f9e3a578e46575eccad661e8725e6aeec49
fatevil <marekoka@seznam.cz>
2020-02-23 11:21:10 +0100

    Spring Init.

---
+++ src/main/java/com/ataccama/restapiapp/RestapiappApplication.java
@@ -0,0 +1,13 @@
+package com.ataccama.restapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiappApplication.class, args);
+	}
+
+}
